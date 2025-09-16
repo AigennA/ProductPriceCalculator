b6ae7e42de5e6d9f77b272aef65e8ca8526f4d44
Aigen80 <aomurm@gmail.com>
2025-09-16 10:22:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ProductPricecalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Price Calculator");
+            Console.WriteLine("How many products do you wanna buy?");
+            var totalProducts = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("How much do you wanna pay for each product");
+            var price = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("How much is the VAT in procent?"); //Asking to user
+            var vat = double.Parse(Console.ReadLine());
+
+            double totalPrice = 0;
+            if (vat <= 0)
+            {
+                totalPrice = CalculatePrice(totalProducts, price);
+            }
+            else
+            {
+                totalPrice = CalculatePrice(totalProducts, price, vat);
+            }
+            Console.WriteLine($"Total price inc. VAT:{totalPrice} kr");
+
+
+        }
+
+        private static double CalculatePrice(
+            int totalProducts,
+            int price,
+            double vat = 0.20)
+        {
+            var totalPrice = (totalProducts) * (1 + vat);
+            return totalPrice;
+        }
+
+    }
+}
